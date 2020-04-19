426d4db1bc130be36b8af5e216857dbdba69d50e
akanksha111-sudo <akanksha.mahajan0@gmail.com>
2020-04-20 00:53:35 +0530

    commit

---
+++ django/notejam/testgauge/mygaugetest/specs/sign-in .spec
@@ -0,0 +1,26 @@
+#Sign in functionality
+
+
+
+
+##Sign in and creation of note
+
+
+* Signup to notejam with "mahajananky@gmail.com"
+* Use the password "123456789"
+* Create a new pad with name "My Daily Routine" and save
+* write a note with name "Monday routine"
+* write inside the note "Wake up at 6:00 am and then do some yoga and meditation after that take some bathe and after that have some light breakfast and then start working and then have some lunch and then continue office work and then do some cardio exercise and then have some dinner and then sleep " and save
+
+
+## Forgot Password
+
+*Click the button Forgot password
+*Write "akanksha.mahajan0@gmail.com" in email box and it will generate a new password
+
+##New Account Sign-up
+
+* Click on sign up to create a new account
+*signup with email "abc@gmail.com" 
+*Enter password as "abc"
+*Confirm the password by again typing "abc" and sign up with a new account
---
+++ django/notejam/testgauge/mygaugetest/tests/step_implementation.js
@@ -0,0 +1,80 @@
+/* globals gauge*/
+"use strict";
+const { openBrowser,write,button, closeBrowser, $ , click , goto,label, link,into , press, screenshot, text, focus, textBox, toRightOf } = require('taiko');
+const assert = require("assert");
+const headless = process.env.headless_chrome.toLowerCase() === 'true';
+const BASE_URL = "http://127.0.0.1:8000/ ";
+
+beforeSuite(async () => {
+    await openBrowser({ headless: headless })
+    await goto(BASE_URL);
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+gauge.screenshotFn = async function() {
+    return await screenshot({ encoding: 'base64' });
+};
+
+step("Signup to notejam with <email>", async function(email) {
+    await goto(BASE_URL);
+    await write(email, into (textBox({name:"email"})));
+
+});
+
+step("Use the password <password>", async function(password) {
+    await write(password, into (textBox({name:"password"})));
+    await click(button('Sign in'));
+    await click(link('New pad'));
+});
+
+
+step("Create a new pad with name <Name> and save", async function(name) {
+    await write(name, into (textBox({ name:"name"})));
+    await click(button('Save'));
+    await click(link('New note'));
+});
+
+step("write a note with name <Name>", async function(name) {
+	await write(name,into(textBox({name:"name"})));
+});
+
+
+
+step("write inside the note <text> and save", async function(text) {
+    await write(text,into(textBox({name:"text"})));
+    await click(button("Save"));
+});
+
+step("Click the button Forgot password", async function() {
+	await click(link("Forgot Password"));
+});
+
+step("Write <email> in email box and it will generate a new password", async function(email) {
+    await write(email,into(textBox({name:"email"})));
+    await click(button("Generate password"));
+});
+
+
+
+
+
+step("Click on sign up to create a new account", async function() {
+	await click(link("Sign up"));
+});
+
+
+step("signup with email <email>", async function(email) {
+	await write(email,into(textBox({name:"email"})));
+});
+
+step("Enter password as <password>", async function(password) {
+	await write(password,into(textBox({name:"password"})));
+});
+
+step("Confirm the password by again typing <repeat_password> and sign up with a new account", async function(repeat_password) {
+    await write(repeat_password,into(textBox({name:"repeat_password"})));
+    await click(button("Sign up"));
+});
